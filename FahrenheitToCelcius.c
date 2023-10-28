87fffec9c530588bc48a5343aa7c7e5ee894d654
Liam Rigney <100379941+LimeKMS@users.noreply.github.com>
2023-10-28 11:39:23 -0400

    Create FahrenheitToCelcius.c

---
+++ FahrenheitToCelcius.c
@@ -0,0 +1,24 @@
+/**********************************************************
+ * This is a function that converts Fahrenheit to Celsius.*
+ * By: Liam Rigney                                        *
+ * Date 9/05/2020                                         *
+ **********************************************************/
+
+#include <stdio.h>
+
+int main(void) {
+
+int specialNum;
+
+     scanf("%d", &specialNum);
+
+
+if (specialNum == -99 || specialNum == 0 || specialNum == 44) {
+         printf("Special number\n");
+     }
+     else {
+          printf("Not special number\n");
+     }
+
+  return 0;
+}
